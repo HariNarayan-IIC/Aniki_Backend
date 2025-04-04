594fb423b055c8b3c23e4d25f24124c11c7b413b
R-Hari-Narayan <harinarayan2002@gmail.com>
2025-04-04 21:44:52 +0530

    Util files asyncHandler, ApiError and ApiResponse file created

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 import 'dotenv/config'
-import express from "express"
 import connectDB from './db/index.js'
+import { app } from './app.js'
 
-
+const port = process.env.PORT || 8000
 
 connectDB()
+.then(() => {
+    app.listen(port, () => {
+        console.log(` ⚙️    Server is running at port : ${port} \n`);
+    })
+})
+.catch((err) => {
+    console.log(" MONGO db connection failed : ", err);
+})
