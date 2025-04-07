02d976bd3ed9db107cce054a2346847d84d2b4ea
R Hari Narayan <rhari.narayan@iic.ac.in>
2025-04-07 22:29:08 +0530

    Cloudinary upload function created

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -12,36 +12,20 @@ cloudinary.config({
 
 
 const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        //Upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        //File has been uploaded
+        console.log("File has been uploaded successfully!!", response.url)
+        return response
+    } catch (error) {
+        fs.unlinkSync(lacalFilePath) //Remove the locally saved temporary file as the upload operation failed
+        return null;
+    }
 
 }
-    
-// Upload an image
-const uploadResult = await cloudinary.uploader
-.upload(
-    'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
-        public_id: 'shoes',
-    }
-)
-.catch((error) => {
-    console.log(error);
-});
 
-console.log(uploadResult);
-
-// Optimize delivery by resizing and applying auto-format and auto-quality
-const optimizeUrl = cloudinary.url('shoes', {
-    fetch_format: 'auto',
-    quality: 'auto'
-});
-
-console.log(optimizeUrl);
-
-// Transform the image: auto-crop to square aspect_ratio
-const autoCropUrl = cloudinary.url('shoes', {
-    crop: 'auto',
-    gravity: 'auto',
-    width: 500,
-    height: 500,
-});
-
-console.log(autoCropUrl);    
+export {uploadOnCloudinary}
