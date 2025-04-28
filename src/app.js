74a2f7341d86cf401473a5f1c73cafc5887a9b97
R-Hari-Narayan <harinarayan2002@gmail.com>
2025-04-28 15:13:01 +0530

    Changed to cors middleware

--- src/app.js
+++ src/app.js
@@ -7,10 +7,24 @@ import logger from "./middlewares/logger.middleware.js"
 const app = express()
 
 //setup middlewares
+const allowedOrigins = [
+    "https://aniki-frontend.netlify.app",
+    "http://localhost:5173", // if you want local frontend too
+  ];
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+    origin: function(origin, callback) {
+        // allow requests with no origin (like mobile apps, curl, Postman)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new ApiError(400, msg), false);
+        }
+        return callback(null, true);
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],  // explicitly allow methods
+    allowedHeaders: ['Content-Type', 'Authorization'],    // explicitly allow headers
+}));
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
@@ -25,6 +39,7 @@ app.get("/", (req, res) => {
 
 //routes import 
 import userRouter from './routes/user.routes.js';
+import { ApiError } from "./utils/ApiError.js"
 
 
 //routes declaration
