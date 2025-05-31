96150307ff49366d092acb4246fee913145768c0
R-Hari-Narayan <harinarayan2002@gmail.com>
2025-05-31 08:53:56 +0530

    Fetch messages for chatRooms added

--- src/controllers/chatroom.controller.js
+++ src/controllers/chatroom.controller.js
@@ -1,4 +1,5 @@
 import ChatRoom from "../models/chatRoom.model.js";
+import ChatMessage from "../models/chatMessage.model.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
@@ -29,4 +30,21 @@ export const createChatRoom = asyncHandler( async(req, res) => {
             "Created new chat room successfully"
         )
     )
+});
+
+export const getAllMessages = asyncHandler(async (req, res) => {
+    const rawMessages = await ChatMessage.find({ roomId: req.params.roomId })
+        .sort({ createdAt: -1 }) // get latest messages
+        .limit(100)
+        .lean(); // improves performance if you don't need Mongoose docs
+
+    const messages = rawMessages.reverse(); // now oldest -> newest
+
+    res.status(200).json(
+        new ApiResponse(
+            200,
+            messages,
+            "Fetched last 100 messages in correct order"
+        )
+    );
 });
--- src/routes/chatroom.routes.js
+++ src/routes/chatroom.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getAllChatRooms, createChatRoom } from '../controllers/chatroom.controller.js';
+import { getAllChatRooms, createChatRoom, getAllMessages } from '../controllers/chatroom.controller.js';
 import { body } from 'express-validator';
 
 
@@ -11,6 +11,7 @@ router.route('/').post(
     body('name').isString().notEmpty(),
     createChatRoom
 );
+router.route('/:roomId').get(getAllMessages);
 
 
 export default router;
